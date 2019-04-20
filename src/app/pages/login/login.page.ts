dc590fe599839321ec35198e2aead9cc512a763f
Oscar Diaz <oscardiaz1201@gmail.com>
2019-04-20 14:49:35 +0200

    Cambios en el proyecto

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  usuario={
+    email:'',
+    password: ''
+  }
+
   constructor() { }
 
   ngOnInit() {
